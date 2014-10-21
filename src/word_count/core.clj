f5500b47f5a90f0b71923ca672f93ea974339d64
tom.vaughan <tom@opower.com>
2014-10-21 11:08:41 -0400

    Renaming "foo" to something more descriptive

--- src/word_count/core.clj
+++ src/word_count/core.clj
@@ -1,17 +1,17 @@
 (ns word-count.core)
 (require ['clojure.string :as 'str])
 
-(defn foo [input-seq hash]
+(defn create-word-count-hash [input-seq hash]
     (let [word    (first input-seq)
           current (get-in hash [word])]
         (cond
             (nil? word)    hash
-            (nil? current) (foo (rest input-seq) (assoc hash word 1))
-            :else          (foo (rest input-seq) (update-in hash [word] inc)))))
+            (nil? current) (create-word-count-hash (rest input-seq) (assoc hash word 1))
+            :else          (create-word-count-hash (rest input-seq) (update-in hash [word] inc)))))
             
 (defn word-count [s]
    (let
         [lower-s (str/lower-case s)
          no-punct (.replaceAll lower-s "[^a-z0-9 ]" "")
          words (.split no-punct "\\W+")]
-        (foo words {})))
+        (create-word-count-hash words {})))
